3f97cb2758957dbea710afac769d393cbe5bd3b6
mohi-devhub <mohith.cvv230152@cvv.ac.in>
2026-05-15 17:06:37 +0530

    chore: redirect /marketplace to /registry

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/marketplace",
+        destination: "/registry",
+        permanent: true,
+      },
+      {
+        source: "/marketplace/:id",
+        destination: "/registry/:id",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
